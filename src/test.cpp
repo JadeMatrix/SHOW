4f5511f34a08866ef74a29bf74981baa7abf116a
Joseph Durel <josephdurel@gmail.com>
2017-09-13 15:51:56 -0400

    Increased test program complexity with a "Server" header and "GET" method

--- src/test.cpp
+++ src/test.cpp
@@ -16,7 +16,40 @@ void basic_request_hander( show::request& request )
 {
     std::cout << "\n\nvvvv IN REQUEST HANDLER vvvv\n\n";
     
-    if( request.unknown_content_length )
+    if( request.method == "GET" )
+    {
+        show::response_code rc = {
+            200,
+            "OK"
+        };
+        show::headers_t headers;
+        
+        std::string get_string(
+            "Response served from "
+            + show::version.name
+            + " v"
+            + show::version.string
+        );
+        
+        headers[ "Content-Length" ].push_back(
+            std::to_string( get_string.size() )
+        );
+        headers[ "Server" ].push_back(
+            show::version.name
+            + " v"
+            + show::version.string
+        );
+        
+        show::response response(
+            request,
+            rc,
+            headers
+        );
+        std::ostream response_stream( &response );
+        
+        response_stream << get_string;
+    }
+    else if( request.unknown_content_length )
     {
         show::response_code rc = {
             400,
@@ -30,6 +63,11 @@ void basic_request_hander( show::request& request )
             std::to_string( message.size() )
         );
         headers[ "Content-Type" ].push_back( "text/plain" );
+        headers[ "Server" ].push_back(
+            show::version.name
+            + " v"
+            + show::version.string
+        );
         
         show::response response(
             request,
@@ -54,6 +92,11 @@ void basic_request_hander( show::request& request )
         
         headers[ "Content-Length" ].push_back(
             std::to_string( remaining_content )
+        );
+        headers[ "Server" ].push_back(
+            show::version.name
+            + " v"
+            + show::version.string
         );
         
         show::response response(
@@ -104,52 +147,12 @@ int main( int argc, char* argv[] )
     
     try
     {
-        // std::string source_string = "こんにちは";
-        // std::string url_encoded_string = show::url_encode( source_string );
-        // std::string url_decoded_string = show::url_decode( url_encoded_string );
-        // std::string base64_encoded_string = show::base64_encode( source_string );
-        // std::string base64_decoded_string = show::base64_decode( base64_encoded_string );
-        
-        // // 44Ck+CgpD44Cq+C
-        // // 44CT44CT44Cr44Ch44Cv
-        // // 44GT44KT44Gr44Gh44Gv
-        // // 44GT44KT44Gr44Gh44Gv
-        
-        // std::cout
-        //     << source_string
-        //     << "\n"
-        //     << url_encoded_string
-        //     << "\n"
-        //     << url_decoded_string
-        //     << "\n"
-        //     << base64_encoded_string
-        //     << "\n"
-        //     << base64_decoded_string
-        //     << "\n"
-        // ;
-        
-        // protoent* tcp = getprotobyname( "TCP" );
-        
-        // std::cout
-        //     << tcp -> p_name
-        //     << " - "
-        //     << tcp -> p_proto
-        //     << "\n"
-        // ;
-        // return 0;
-        
-        // DEBUG:
-        std::cout << "preparing to create server\n";
-        
         show::server test_server(
             basic_request_hander,
             argv[ 1 ],
             std::stoi( argv[ 2 ] ),
             -1
         );
-        
-        // DEBUG:
-        std::cout << "server created\n";
         
         test_server.serve();
     }
