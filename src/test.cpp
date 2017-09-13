2f94abb8cdf4ee6698467df971d9ae690b6c9c76
Joseph Durel <josephdurel@gmail.com>
2017-09-13 17:02:11 -0400

    Consolidated cases in test program

--- src/test.cpp
+++ src/test.cpp
@@ -16,109 +16,105 @@ void basic_request_hander( show::request& request )
 {
     std::cout << "\n\nvvvv IN REQUEST HANDLER vvvv\n\n";
     
+    show::response_code rc = {
+        200,
+        "OK"
+    };
+    show::headers_t headers;
+    
+    headers[ "Server" ].push_back(
+        show::version.name
+        + " v"
+        + show::version.string
+    );
+    
+    std::string response_content;
+    std::streambuf* response_content_buf = nullptr;
+    
     if( request.method == "GET" )
     {
-        show::response_code rc = {
-            200,
-            "OK"
-        };
-        show::headers_t headers;
-        
-        std::string get_string(
+        response_content =
             "Response served from "
             + show::version.name
             + " v"
             + show::version.string
-        );
+        ;
         
-        headers[ "Content-Length" ].push_back(
-            std::to_string( get_string.size() )
-        );
-        headers[ "Server" ].push_back(
-            show::version.name
-            + " v"
-            + show::version.string
-        );
-        
-        show::response response(
-            request,
-            rc,
-            headers
-        );
-        std::ostream response_stream( &response );
-        
-        response_stream << get_string;
-    }
-    else if( request.unknown_content_length )
-    {
-        show::response_code rc = {
-            400,
-            "Bad Request"
-        };
-        show::headers_t headers;
-        
-        std::string message = "Missing \"Content-Length\" header";
-        
-        headers[ "Content-Length" ].push_back(
-            std::to_string( message.size() )
-        );
         headers[ "Content-Type" ].push_back( "text/plain" );
-        headers[ "Server" ].push_back(
-            show::version.name
-            + " v"
-            + show::version.string
-        );
-        
-        show::response response(
-            request,
-            rc,
-            headers
-        );
-        std::ostream response_stream( &response );
-        response_stream << message;
+    }
+    else if( request.method == "POST" )
+    {
+        if( request.unknown_content_length )
+        {
+            rc.code = 400;
+            rc.description = "Bad Request";
+            
+            response_content = "Missing \"Content-Length\" header";
+            
+            headers[ "Content-Type" ].push_back( "text/plain" );
+        }
+        else
+        {
+            response_content_buf = &request;
+            
+            headers[ "Content-Length" ].push_back(
+                std::to_string( request.content_length )
+            );
+            
+            auto content_type_header = request.headers.find( "Content-Type" );
+            if(
+                content_type_header != request.headers.end()
+                && content_type_header -> second.size() == 1
+            )
+                headers[ "Content-Type" ].push_back(
+                    content_type_header -> second[ 0 ]
+                );
+            else
+                headers[ "Content-Type" ].push_back( "text/plain" );
+        }
     }
     else
     {
-        show::response_code rc = {
-            200,
-            "OK"
-        };
-        show::headers_t headers;
+        rc.code = 501;
+        rc.description = "Not Implemented";
+    }
+    
+    show::response response(
+        request,
+        rc,
+        headers
+    );
+    
+    std::ostream response_stream( &response );
+    
+    if( response_content_buf == nullptr )
+    {
+        headers[ "Content-Length" ].push_back(
+            std::to_string( response_content.size() )
+        );
         
-        std::istream request_stream( &request );
+        response_stream.write(
+            response_content.c_str(),
+            response_content.size()
+        );
+    }
+    else
+    {
+        std::istream response_content_stream( response_content_buf );
+        
         char iobuff[ 256 ];
         
-        unsigned long long remaining_content = request.content_length;
-        
-        headers[ "Content-Length" ].push_back(
-            std::to_string( remaining_content )
-        );
-        headers[ "Server" ].push_back(
-            show::version.name
-            + " v"
-            + show::version.string
-        );
-        
-        show::response response(
-            request,
-            rc,
-            headers
-        );
-        std::ostream response_stream( &response );
-        
-        while( remaining_content > 0 )
+        while( response_content_stream.good() )
         {
-            std::streamsize read_count = request_stream.readsome(
+            std::streamsize read_count = response_content_stream.readsome(
                 iobuff,
-                remaining_content < 256 ? remaining_content : 256
+                256
             );
             
             response_stream.write(
                 iobuff,
                 read_count
             );
-            
-            remaining_content -= read_count;
         }
     }
     
