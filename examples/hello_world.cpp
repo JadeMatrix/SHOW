79aa7aef8523d59bf08152f2e734a4a6b6c94272
Joseph Durel <josephdurel@gmail.com>
2017-10-25 16:24:04 -0400

    Added Hello World example

---
+++ examples/hello_world.cpp
@@ -0,0 +1,100 @@
+#include <show.hpp>
+
+#include <iostream> // std::cerr
+#include <string>   // std::to_string()
+
+
+int main( int argc, char* argv[] )
+{
+    std::string  host    = "::";    // IPv6 loopback (0.0.0.0 in IPv4)
+    unsigned int port    = 9090;    // Some random higher port
+    int          timeout = 10;      // Connection timeout in seconds
+    std::string  message = "Hello World!";
+    
+    try
+    {
+        show::server test_server(
+            host,
+            port,
+            timeout
+        );
+        
+        while( true )
+            try
+            {
+                show::connection connection( test_server.serve() );
+                
+                try
+                {
+                    // This simple program doesn't even bother reading requests,
+                    // but a request object is still needed to create a response
+                    show::request request( connection );
+                    
+                    show::response_code code = {
+                        501,
+                        "Not Implemented"
+                    };
+                    show::headers_t headers = {
+                        // Set the Server header to display the SHOW version
+                        { "Server", {
+                            show::version.name
+                            + " v"
+                            + show::version.string
+                        } },
+                        // The message is simple plain text
+                        { "Content-Type", { "text/plain" } },
+                        { "Content-Length", {
+                            // Header values must be strings
+                            std::to_string( message.size() )
+                        } }
+                    };
+                    
+                    show::response response(
+                        request,
+                        show::http_protocol::HTTP_1_0,
+                        code,
+                        headers
+                    );
+                    
+                    response.sputn( message.c_str(), message.size() );
+                    // Alternatively a std::ostream could be created using the
+                    // response as a buffer, and then the message sent using <<
+                    // or write()
+                }
+                catch( show::connection_timeout& ct )
+                {
+                    std::cout
+                        << "timed out waiting on client, closing connection"
+                        << std::endl
+                    ;
+                    break;
+                }
+            }
+            catch( show::connection_timeout& ct )
+            {
+                std::cout
+                    << "timed out waiting for connection, looping..."
+                    << std::endl
+                ;
+            }
+    }
+    catch( show::exception& e )
+    {
+        std::cerr
+            << "uncaught exception in main(): "
+            << e.what()
+            << std::endl
+        ;
+        return -1;
+    }
+    catch( ... )
+    {
+        std::cerr
+            << "uncaught non-std::exception in main()"
+            << std::endl
+        ;
+        return -1;
+    }
+    
+    return 0;
+}
