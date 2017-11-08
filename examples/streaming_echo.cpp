53ae7aa90a0d80b1e887efd438b110924b93ca58
Joseph Durel <josephdurel@gmail.com>
2017-11-07 23:19:56 -0500

    Reorganized `streaming_echo` a bit to improve readability

--- examples/streaming_echo.cpp
+++ examples/streaming_echo.cpp
@@ -19,6 +19,97 @@ const show::headers_t::value_type server_header = {
 const std::streamsize buffer_size = 256;
 
 
+void handle_POST_request( show::request& request )
+{
+    // Always require a Content-Length header for this application
+    if( request.unknown_content_length )
+        show::response response(
+            request,
+            show::http_protocol::HTTP_1_0,
+            { 400, "Bad Request" },
+            { server_header }
+        );
+    else
+    {
+        show::headers_t headers = {
+            {
+                "Content-Length",
+                // Header values must be strings
+                { std::to_string( request.content_length ) }
+            }
+        };
+        
+        // Replicate the Content-Type header of the request if it exists,
+        // otherwise assume plain text
+        auto content_type_header = request.headers.find( "Content-Type" );
+        if(
+            content_type_header != request.headers.end()
+            && content_type_header -> second.size() == 1
+        )
+            headers[ "Content-Type" ].push_back(
+                content_type_header -> second[ 0 ]
+            );
+        else
+            headers[ "Content-Type" ].push_back( "text/plain" );
+        
+        // Start a response before we read any data
+        show::response response(
+            request,
+            show::http_protocol::HTTP_1_0,
+            { 200, "OK" },
+            headers
+        );
+        
+        // Create stream objects for request & response, as well as a read
+        // buffer for the echoed contents
+        std::istream  request_stream( &request  );
+        std::ostream response_stream( &response );
+        char buffer[ buffer_size ];
+        
+        // Note that std::istream::eof() only returns true after the first read
+        // operation to fail because of EOF.  Note you could instead use
+        // `show::request::oef()`, but this is meant to demonstrate using the
+        // stdlib stream objects.
+        do
+        {
+            // Block until we can read at least one byte or we reach the end.
+            // This is to prevent the server from going into a busy loop while
+            // waiting for content from the client.
+            buffer[ 0 ] = request_stream.get();
+            
+            // `std::istream` & `std::ostream` swallow all exceptions and set
+            // `badbit`, so code that uses streams for reading & writing will
+            // never actually be able to catch a `show::client_disconnected` or
+            // `show::connection_timeout` during those operations.
+            if( !request_stream.good() )
+            {
+                std::cout
+                    << "failed to read data from client "
+                    << request.client_address
+                    << ", disconnecting"
+                    << std::endl
+                ;
+                continue;
+            }
+            
+            // Read additional available bytes up to the end of the buffer
+            std::streamsize read_count = request_stream.readsome(
+                buffer + 1,
+                buffer_size - 1
+            );
+            
+            // Write all the read bytes back to the response
+            response_stream.write(
+                buffer,
+                read_count + 1
+            );
+            // Most likely don't want to manually flush in production code, but
+            // this allows you see the streaming in action
+            response.flush();
+        } while( request_stream.good() && response_stream.good() );
+    }
+}
+
 int main( int argc, char* argv[] )
 {
     std::string  host    = "::";    // IPv6 loopback (0.0.0.0 in IPv4)
@@ -55,101 +146,7 @@ int main( int argc, char* argv[] )
                         continue;
                     }
                     
-                    // Always require a Content-Length header for this
-                    // application
-                    if( request.unknown_content_length )
-                    {
-                        show::response response(
-                            request,
-                            show::http_protocol::HTTP_1_0,
-                            { 400, "Bad Request" },
-                            { server_header }
-                        );
-                        continue;
-                    }
-                    
-                    show::headers_t headers = {
-                        {
-                            "Content-Length",
-                            // Header values must be strings
-                            { std::to_string( request.content_length ) }
-                        }
-                    };
-                    
-                    // Replicate the Content-Type header of the request if it exists,
-                    // otherwise assume plain text
-                    auto content_type_header = request.headers.find( "Content-Type" );
-                    if(
-                        content_type_header != request.headers.end()
-                        && content_type_header -> second.size() == 1
-                    )
-                        headers[ "Content-Type" ].push_back(
-                            content_type_header -> second[ 0 ]
-                        );
-                    else
-                        headers[ "Content-Type" ].push_back( "text/plain" );
-                    
-                    // Start a response before we read any data
-                    show::response response(
-                        request,
-                        show::http_protocol::HTTP_1_0,
-                        {
-                            200,
-                            "OK"
-                        },
-                        headers
-                    );
-                    
-                    // Create stream objects for request & response, as well as
-                    // a read buffer for the echoed contents
-                    std::istream  request_stream( &request  );
-                    std::ostream response_stream( &response );
-                    char buffer[ buffer_size ];
-                    
-                    // Note that std::istream::eof() only returns true after the
-                    // first read operation to fail because of EOF.  Note you
-                    // could instead use `show::request::oef()`, but this is
-                    // meant to demonstrate using the stdlib stream objects.
-                    do
-                    {
-                        // Block until we can read at least one byte or we reach
-                        // the end.  This is to prevent the server from going
-                        // into a busy loop while waiting for content from the
-                        // client.
-                        buffer[ 0 ] = request_stream.get();
-                        // `std::istream` & `std::ostream` swallow all
-                        // exceptions and set `badbit`, so code that uses
-                        // streams for reading & writing will never actually be
-                        // able to catch a `show::client_disconnected` or
-                        // `show::connection_timeout` during those operations.
-                        if( !request_stream.good() )
-                        {
-                            std::cout
-                                << "failed to read data from client "
-                                << connection.client_address
-                                << ", disconnecting"
-                                << std::endl
-                            ;
-                            continue;
-                        }
-                        
-                        // Read additional available bytes up to the end of the
-                        // buffer
-                        std::streamsize read_count = request_stream.readsome(
-                            buffer + 1,
-                            buffer_size - 1
-                        );
-                        
-                        // Write all the read bytes back to the response
-                        response_stream.write(
-                            buffer,
-                            read_count + 1
-                        );
-                        // Most likely don't want to manually flush in
-                        // production code, but this allows you see the
-                        // streaming in action
-                        response.flush();
-                    } while( request_stream.good() && response_stream.good() );
+                    handle_POST_request( request );
                 }
                 catch( show::client_disconnected& cd )
                 {
