538d8406bf810b5fa3d5b5baba303b45c5394364
Joseph Durel <josephdurel@gmail.com>
2017-11-17 13:08:55 -0500

    Moved base64 utilities to their own header file, as core SHOW has no dependencies on them

---
+++ src/show/base64.hpp
@@ -0,0 +1,211 @@
+#pragma once
+#ifndef SHOW_BASE64_HPP
+#define SHOW_BASE64_HPP
+
+
+#include "../show.hpp"
+
+
+namespace show
+{
+    const char* base64_chars_standard =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    const char* base64_chars_urlsafe  =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    
+    
+    std::string base64_encode(
+        const std::string& o,
+        const char* chars = base64_chars_standard
+    ) noexcept;
+    std::string base64_decode(
+        const std::string& o,
+        const char* chars = base64_chars_standard
+    );
+    
+    
+    class base64_decode_error : public exception {
+        using exception::exception;
+    };
+    
+    
+    std::string base64_encode(
+        const std::string& o,
+        const char* chars
+    ) noexcept
+    {
+        unsigned char current_sextet;
+        std::string encoded;
+        
+        std::string::size_type b64_size = ( ( o.size() + 2 ) / 3 ) * 4;
+        
+        for(
+            std::string::size_type i = 0, j = 0;
+            i < b64_size;
+            ++i
+        )
+        {
+            switch( i % 4 )
+            {
+            case 0:
+                // j
+                // ******** ******** ********
+                // ^^^^^^
+                // i
+                current_sextet = ( o[ j ] >> 2 ) & 0x3F /* 00111111 */;
+                encoded += chars[ current_sextet ];
+                break;
+            case 1:
+                // j        j++
+                // ******** ******** ********
+                //       ^^ ^^^^
+                //       i
+                current_sextet = ( o[ j ] << 4 ) & 0x30 /* 00110000 */;
+                ++j;
+                if( j < o.size() )
+                    current_sextet |= ( o[ j ] >> 4 ) & 0x0F /* 00001111 */;
+                encoded += chars[ current_sextet ];
+                break;
+            case 2:
+                //          j        j++
+                // ******** ******** ********
+                //              ^^^^ ^^
+                //              i
+                if( j < o.size() )
+                {
+                    current_sextet = ( o[ j ] << 2 ) & 0x3C /* 00111100 */;
+                    ++j;
+                    if( j < o.size() )
+                        current_sextet |= ( o[ j ] >> 6 ) & 0x03 /* 00000011 */;
+                    encoded += chars[ current_sextet ];
+                }
+                else
+                    encoded += '=';
+                break;
+            case 3:
+                //                   j
+                // ******** ******** ********
+                //                     ^^^^^^
+                //                     i
+                if( j < o.size() )
+                {
+                    current_sextet = o[ j ] & 0x3F /* 00111111 */;
+                    ++j;
+                    encoded += chars[ current_sextet ];
+                }
+                else
+                    encoded += '=';
+                break;
+            }
+        }
+        
+        return encoded;
+    }
+    
+    std::string base64_decode( const std::string& o, const char* chars )
+    {
+        /*unsigned*/ char current_octet;
+        std::string decoded;
+        
+        std::string::size_type unpadded_len = o.size();
+        
+        for(
+            std::string::const_reverse_iterator r_iter = o.rbegin();
+            r_iter != o.rend();
+            ++r_iter
+        )
+        {
+            if( *r_iter == '=' )
+                --unpadded_len;
+            else
+                break;
+        }
+        
+        std::string::size_type b64_size = unpadded_len;
+        
+        if( b64_size % 4 )
+            b64_size += 4 - ( b64_size % 4 );
+        
+        if( b64_size > o.size() )
+            // Missing required padding
+            // TODO: add flag to explicitly ignore?
+            throw base64_decode_error( "missing required padding" );
+        
+        std::map< char, /*unsigned*/ char > reverse_lookup;
+        for( /*unsigned*/ char i = 0; i < 64; ++i )
+            reverse_lookup[ chars[ i ] ] = i;
+        reverse_lookup[ '=' ] = 0;
+        
+        for( std::string::size_type i = 0; i < b64_size; ++i )
+        {
+            if( o[ i ] == '=' )
+            {
+                if(
+                    i < unpadded_len
+                    && i >= unpadded_len - 2
+                )
+                    break;
+                else
+                    throw base64_decode_error( "premature padding" );
+            }
+            
+            std::map< char, /*unsigned*/ char >::iterator first, second;
+            
+            first = reverse_lookup.find( o[ i ] );
+            if( first == reverse_lookup.end() )
+                throw base64_decode_error( "invalid base64 character" );
+            
+            if( i + 1 < o.size() )
+            {
+                second = reverse_lookup.find( o[ i + 1 ] );
+                if( second == reverse_lookup.end() )
+                    throw base64_decode_error( "invalid base64 character" );
+            }
+            
+            switch( i % 4 )
+            {
+            case 0:
+                // i
+                // ****** ****** ****** ******
+                // ^^^^^^ ^^
+                current_octet = first -> second << 2;
+                if( i + 1 < o.size() )
+                    current_octet |= (
+                        second -> second >> 4
+                    ) & 0x03 /* 00000011 */;
+                decoded += current_octet;
+                break;
+            case 1:
+                //        i
+                // ****** ****** ****** ******
+                //          ^^^^ ^^^^
+                current_octet = first -> second << 4;
+                if( i + 1 < o.size() )
+                    current_octet |= (
+                        second -> second >> 2
+                    ) & 0x0F /* 00001111 */;
+                decoded += current_octet;
+                break;
+            case 2:
+                //               i
+                // ****** ****** ****** ******
+                //                   ^^ ^^^^^^
+                current_octet = ( first -> second << 6 ) & 0xC0 /* 11000000 */;
+                if( i + 1 < o.size() )
+                    current_octet |= second -> second & 0x3F /* 00111111 */;
+                decoded += current_octet;
+                break;
+            case 3:
+                //                      i
+                // ****** ****** ****** ******
+                // -
+                break;
+            }
+        }
+        
+        return decoded;
+    }
+}
+
+
+#endif
