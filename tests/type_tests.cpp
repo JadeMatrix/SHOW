50cfb8b2c1577f97ad894c5e2d27acdfc690e49a
Joseph Durel <josephdurel@gmail.com>
2018-02-01 14:51:07 -0500

    Added unit tests for utility types & constants

---
+++ tests/type_tests.cpp
@@ -0,0 +1,67 @@
+#include <UnitTest++/UnitTest++.h>
+#include <show.hpp>
+
+#include <string>
+
+
+SUITE( ShowTypeTests )
+{
+    TEST( VersionStringCorrect )
+    {
+        CHECK_EQUAL(
+            (
+                  std::to_string( show::version::major )
+                + "."
+                + std::to_string( show::version::minor )
+                + "."
+                + std::to_string( show::version::revision )
+            ),
+            show::version::string
+        );
+    }
+    
+    TEST( ASCIIUpperCase )
+    {
+        std::string string = "Hello World";
+        std::string string_uppercase = show::_ASCII_upper( string );
+        CHECK_EQUAL(
+            "HELLO WORLD",
+            string_uppercase
+        );
+    }
+    
+    TEST( CaseInsensitiveHeadersCreate )
+    {
+        show::headers_type test_headers{
+            { "test", { "foo" } },
+            { "Test", { "bar" } }
+        };
+        CHECK_EQUAL(
+            1,
+            test_headers.size()
+        );
+    }
+    
+    TEST( CaseInsensitiveHeadersAssign )
+    {
+        show::headers_type test_headers{
+            { "test", { "foo" } }
+        };
+        test_headers[ "Test" ] = { "bar" };
+        CHECK_EQUAL(
+            1,
+            test_headers.size()
+        );
+    }
+    
+    TEST( CaseInsensitiveHeadersAccess )
+    {
+        show::headers_type test_headers{
+            { "Test", { "foo" } }
+        };
+        CHECK_EQUAL(
+            true,
+            test_headers.find( "test" ) != test_headers.end()
+        );
+    }
+}
