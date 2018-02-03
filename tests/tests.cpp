bd90f7654141fb31e4fdf24f5eb869a0eb7f8937
Joseph Durel <josephdurel@gmail.com>
2018-02-02 19:45:25 -0500

    Added unit tests for `show::connection` class

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,9 +2,13 @@
 
 #include <curl/curl.h>
 
+#include <cstdlib>  // std::srand()
+#include <ctime>    // std::time()
+
 
 int main( int argc, char* argv[] )
 {
+    std::srand( std::time( nullptr ) );
     curl_global_init( CURL_GLOBAL_ALL );
     int failed = UnitTest::RunAllTests();
     curl_global_cleanup();
