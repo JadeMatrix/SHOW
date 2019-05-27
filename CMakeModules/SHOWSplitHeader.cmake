9c8ea3ac991f019985678a24700eebc6a63dbac4
Joseph Durel <josephdurel@gmail.com>
2019-05-27 12:53:18 -0400

    Cleaned up header split script

--- CMakeModules/SHOWSplitHeader.cmake
+++ CMakeModules/SHOWSplitHeader.cmake
@@ -1,7 +1,7 @@
-# TODO: Clean this mess up
-
+# Make sure target files don't exist so they can safely be appended to
 FILE( REMOVE "${HEADER_FILENAME}" "${SOURCE_FILENAME}" )
 
+# Include generated headers in source file..
 FILE(
     WRITE "${SOURCE_FILENAME}"
     "#include \"${EXPORT_FILENAME}\"\n#include \"${HEADER_FILENAME}\"\n\n"
@@ -14,29 +14,35 @@ SET( SYM_END   "\n// @SHOW_CPP_END\n\n"   )
 STRING( LENGTH "${SYM_BEGIN}" SYM_LEN_BEGIN )
 STRING( LENGTH "${SYM_END}"   SYM_LEN_END   )
 
+# Utility macro, relies on variables defined in the loop below
+MACRO( WRITE_HEADER_CONTENTS_SKIP FILENAME SYM_LEN )
+    # Extract up until the symbol as content-to-write
+    STRING( SUBSTRING "${HEADER_CONTENTS}" 0 "${END_POS}" CONTENT_TO_WRITE )
+    FILE( APPEND "${FILENAME}" "${CONTENT_TO_WRITE}" )
+    
+    # Extract past symbol to the end as the remaining content to read
+    MATH( EXPR SYM_END_POS "${END_POS} + ${SYM_LEN}")
+    STRING( SUBSTRING "${HEADER_CONTENTS}" "${SYM_END_POS}" -1 HEADER_CONTENTS )
+ENDMACRO()
+
 WHILE( 1 )
+    # Find CPP content start & write everything before to header file
     STRING( FIND "${HEADER_CONTENTS}" "${SYM_BEGIN}" END_POS )
-    IF( END_POS LESS "0" )
+    IF( END_POS LESS 0 )
         FILE( APPEND "${HEADER_FILENAME}" "${HEADER_CONTENTS}" )
         BREAK()
     ENDIF()
-    STRING( SUBSTRING "${HEADER_CONTENTS}" "0" "${END_POS}" HEADER_TO_WRITE )
-    MATH( EXPR SYM_END_POS "${END_POS} + ${SYM_LEN_BEGIN}")
-    STRING( SUBSTRING "${HEADER_CONTENTS}" "${SYM_END_POS}" "-1" HEADER_CONTENTS )
+    WRITE_HEADER_CONTENTS_SKIP( "${HEADER_FILENAME}" "${SYM_LEN_BEGIN}" )
     
-    FILE( APPEND "${HEADER_FILENAME}" "${HEADER_TO_WRITE}" )
-    
+    # Find CPP content end & write everything before to source file
     STRING( FIND "${HEADER_CONTENTS}" "${SYM_END}" END_POS )
-    IF( END_POS LESS "0" )
+    IF( END_POS LESS 0 )
         MESSAGE( ERROR "Missing @SHOW_CPP_END comment in ${ORIG_FILENAME}" )
     ENDIF()
-    STRING( SUBSTRING "${HEADER_CONTENTS}" "0" "${END_POS}" SOURCE_TO_WRITE )
-    MATH( EXPR SYM_END_POS "${END_POS} + ${SYM_LEN_END}")
-    STRING( SUBSTRING "${HEADER_CONTENTS}" "${SYM_END_POS}" "-1" HEADER_CONTENTS )
-    
-    FILE( APPEND "${SOURCE_FILENAME}" "${SOURCE_TO_WRITE}" )
+    WRITE_HEADER_CONTENTS_SKIP( "${SOURCE_FILENAME}" "${SYM_LEN_END}" )
 ENDWHILE()
 
+# Replace all `inline`s with exports
 FILE( READ "${SOURCE_FILENAME}" SOURCE_CONTENTS )
 STRING( REPLACE "inline" "SHOW_EXPORT" SOURCE_CONTENTS "${SOURCE_CONTENTS}" )
 FILE( WRITE "${SOURCE_FILENAME}" "${SOURCE_CONTENTS}" )
